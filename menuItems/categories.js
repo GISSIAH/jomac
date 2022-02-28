02ea47f88d05d4f17ebe2abcdb9f9f6406ef85d6
GISSIAH <62018768+GISSIAH@users.noreply.github.com>
2022-02-28 13:43:05 +0200

    added descriptions

--- menuItems/categories.js
+++ menuItems/categories.js
@@ -10,13 +10,13 @@ export const categories=[
         title:"Electrical",
         image:"https://res.cloudinary.com/attic-gis/image/upload/v1645779480/196-1962342_power-circuit-breaker-png-circuit-breaker-20a-3_vctlm5_1_vjrrkb.png",
         url:"/categories/Electrical",
-        body:"Browse pvc pipes, Toilet pan sets, Hand basin with pedestrial Kitchen sink, Head basin mixers , Bottle traps 40mm  , Shower arm with rose and many more."
+        body:"Browse meter boards, distribution boards, D irons, pvc pipes, pvc saddles, pvc couplings, pvc boxes, wire nails and many more. "
     },
     {
         title:'Wiring',
         image:"https://res.cloudinary.com/attic-gis/image/upload/v1645779479/Daco_2371438_h0uhaz_1_o1rbfs.png",
         url:"/categories/Wiring",
-        body:"Browse pvc pipes, Toilet pan sets, Hand basin with pedestrial Kitchen sink, Head basin mixers , Bottle traps 40mm  , Shower arm with rose and many more."
+        body:"Browse 1.5 mm red and black wires; 2.5mm red wire, black and green wires; 6mm red and black wires; 10mm red and black wires; and many more."
     },
     {
         title:"Building",
