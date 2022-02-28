8daf4eed176717e8d88aa457c8f682cd069cb3e2
GISSIAH <62018768+GISSIAH@users.noreply.github.com>
2022-02-28 13:57:14 +0200

    updated images to webp

--- menuItems/categories.js
+++ menuItems/categories.js
@@ -2,25 +2,25 @@
 export const categories=[
     {
         title:"Plumbing",
-        image:"https://res.cloudinary.com/attic-gis/image/upload/v1645779483/plumbing_1_semvtb_1_ukyc3e.png",
+        image:"https://res.cloudinary.com/attic-gis/image/upload/v1646049355/jomac/plumbing_1_semvtb_1_ukyc3e_gejhcb.webp",
         url:"/categories/Plumbing",
         body:"Browse pvc pipes, Toilet pan sets, Hand basin with pedestrial Kitchen sink, Head basin mixers , Bottle traps 40mm  , Shower arm with rose and many more."
     },
     {
         title:"Electrical",
-        image:"https://res.cloudinary.com/attic-gis/image/upload/v1645779480/196-1962342_power-circuit-breaker-png-circuit-breaker-20a-3_vctlm5_1_vjrrkb.png",
+        image:"https://res.cloudinary.com/attic-gis/image/upload/v1646049364/jomac/196-1962342_power-circuit-breaker-png-circuit-breaker-20a-3_vctlm5_1_vjrrkb_sgnzpm.webp",
         url:"/categories/Electrical",
         body:"Browse meter boards, distribution boards, D irons, pvc pipes, pvc saddles, pvc couplings, pvc boxes, wire nails and many more. "
     },
     {
         title:'Wiring',
-        image:"https://res.cloudinary.com/attic-gis/image/upload/v1645779479/Daco_2371438_h0uhaz_1_o1rbfs.png",
+        image:"https://res.cloudinary.com/attic-gis/image/upload/v1646049360/jomac/Daco_2371438_h0uhaz_1_o1rbfs_tlvtr8.webp",
         url:"/categories/Wiring",
         body:"Browse 1.5 mm red and black wires; 2.5mm red wire, black and green wires; 6mm red and black wires; 10mm red and black wires; and many more."
     },
     {
         title:"Building",
-        image:"https://res.cloudinary.com/attic-gis/image/upload/v1645779481/PngItem_3503598_hlp105_1_rb2pz9.png",
+        image:"https://res.cloudinary.com/attic-gis/image/upload/v1646049348/jomac/PngItem_3503598_hlp105_1_rb2pz9_fowumn.webp",
         url:"/categories/Building",
         body:"Browse pvc pipes, Toilet pan sets, Hand basin with pedestrial Kitchen sink, Head basin mixers , Bottle traps 40mm  , Shower arm with rose and many more."
         
